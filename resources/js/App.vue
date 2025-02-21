b7fd052decec6fe5f43071af6cdb1af078b45666
Jandl Dávid <jandl.david@miskolcholding.hu>
2025-02-21 20:30:11 +0100

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Üdvözöllek a Laravel és Vue integrációban!</h1>
+</template>
+
+<script setup>
+// Itt adhatsz hozzá JavaScript funkciókat, ha szükséges.
+</script>
+
+<style>
+/* Itt adhatsz hozzá stílusokat */
+</style>
