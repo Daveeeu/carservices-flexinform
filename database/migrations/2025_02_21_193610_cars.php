3c8de992f89f764177e2166fbcd3bc529ecd6d9d
Jandl Dávid <jandl.david@miskolcholding.hu>
2025-02-22 08:49:48 +0100

    fix: Remove unique constraint from 'car_id' in 'cars' table migration

--- database/migrations/2025_02_21_193610_cars.php
+++ database/migrations/2025_02_21_193610_cars.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
-            $table->string('car_id')->unique();
+            $table->string('car_id');
             $table->string('type');
             $table->dateTime('registered');
             $table->boolean('ownbrand');
