206ccee7a0621167f4729500db5fea330ff41001
Jandl Dávid <jandl.david@miskolcholding.hu>
2025-02-23 18:48:39 +0100

    Add pagination to Service Log modal

--- resources/js/components/ServiceLogModal.vue
+++ resources/js/components/ServiceLogModal.vue
@@ -16,9 +16,8 @@
                             <th>Document ID</th>
                         </tr>
                         </thead>
-<!--                        TODO túl sok a log akkor itt is legyen pagination-->
-                        <tbody v-if="services.length > 0">
-                        <tr v-for="service in services" :key="service.log_number">
+                        <tbody v-if="paginatedServices.length > 0">
+                        <tr v-for="service in paginatedServices" :key="service.log_number">
                             <td>{{ service.log_number }}</td>
                             <td>{{ service.event }}</td>
                             <td>{{ service.event_time || "N/A" }}</td>
@@ -29,6 +28,29 @@
                         <tr><td colspan="4" class="text-center">No service logs available for this car.</td></tr>
                         </tbody>
                     </table>
+
+                    <!-- Pagination Controls -->
+                    <div v-if="services.length > pageSize" class="d-flex justify-content-start mt-3">
+                        <nav aria-label="Page navigation">
+                            <ul class="pagination">
+                                <li class="page-item" :class="{ disabled: currentPage === 1 }">
+                                    <button class="page-link" @click="changePage(currentPage - 1)">Previous</button>
+                                </li>
+                                <li
+                                    class="page-item"
+                                    v-for="page in totalPages"
+                                    :key="page"
+                                    :class="{ active: currentPage === page }"
+                                >
+                                    <button class="page-link" @click="changePage(page)">{{ page }}</button>
+                                </li>
+                                <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+                                    <button class="page-link" @click="changePage(currentPage + 1)">Next</button>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" @click="$emit('close-modal')">Close</button>
@@ -44,6 +66,29 @@ export default {
         services: {
             type: Array,
             required: true,
+        },
+    },
+    data() {
+        return {
+            currentPage: 1,
+            pageSize: 5,
+        };
+    },
+    computed: {
+        paginatedServices() {
+            const start = (this.currentPage - 1) * this.pageSize;
+            const end = start + this.pageSize;
+            return this.services.slice(start, end);
+        },
+        totalPages() {
+            return Math.ceil(this.services.length / this.pageSize);
+        },
+    },
+    methods: {
+        changePage(page) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.currentPage = page;
+            }
         },
     },
 };
